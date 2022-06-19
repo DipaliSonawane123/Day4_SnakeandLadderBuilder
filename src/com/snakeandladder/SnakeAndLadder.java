019b53707cd929f81f0cc8463f54db3d02cba982
Dipali <dipssonawane123@gmail.com>
2022-06-19 14:00:25 +0530

    Modified:Two player functionality modified

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -1,47 +1,82 @@
-/* Report the number of times the dice was played to win the game
-and also the position after every die role  */
+/* Play the game with 2Player. In this case if a Player gets a Ladder then plays again.
+Finally, report which Player won the game */
 
 package com.snakeandladder;
 public class SnakeAndLadder {
     final static int source = 0;
     final  static  int destination = 100;
     public static void main(String[] args) {
-        int position=0,diceRoll=0;
-        while(position != destination)
+        int position1=0,diceRoll1=0,position2=0,diceRoll2=0;
+        while(position1 < destination && position2 < destination)
         {
-            diceRoll++;
-            int randomCheck = (int) Math.floor(Math.random() * 10) % 6+1;
-            int  randomCheck2 = (int) (Math.random() * 10) % 3;
-            switch(randomCheck2) {
+            diceRoll1++;
+          int  randomCheck = (int) Math.floor(Math.random() * 10) % 6+1;
+             int randomPlay = (int) (Math.random() * 10) % 3;
+            switch(randomPlay) {
                 case 0:
                     System.out.println("No play");
-                    position += 0;
+                    position1 += 0;
                     break;
                 case 1:
                     System.out.println("Ladder");
-                    position += randomCheck;
+                    position1 += randomCheck;
                     break;
                 case 2:
                     System.out.println("Snake");
-                    position -= randomCheck;
+                    position1 -= randomCheck;
                     break;
             }
-            if (position == 100) {
+            if (position1 == 100) {
                 break;
-            } else if (position > 100) {
-                position -= randomCheck;
-                System.out.println("Not valid Dice Face. You are staying on same position.");
-            } else if (position < 0) {
-                position = source;
+            } else if (position1 > 100) {
+                position1 -= randomCheck;
+                System.out.println("Invalid Dice Face. You are staying on same position.");
+            } else if (position1 < 0) {
+                position1 = source;
                 System.out.println("You came back to start.");
             } else {
-                System.out.println("New Position: " + position);
+                System.out.println("New Position player1: " + position1);
+            }
+
+            diceRoll2++;
+            int randomCheck1 = (int) Math.floor(Math.random() * 10) % 6+1;
+            int randomPlay1 = (int) (Math.random() * 10) % 3;
+            switch(randomPlay1) {
+                case 0:
+                    System.out.println("No play");
+                    position2+=0;
+                    break;
+                case 1:
+                    System.out.println("Ladder");
+                    position2 += randomCheck1;
+                    break;
+                case 2:
+                    System.out.println("Snake");
+                    position2 -= randomCheck1;
+                    break;
+            }
+            if (position2 == 100) {
+                break;
+            } else if (position2 > 100) {
+                position2 -= randomCheck1;
+                System.out.println("Not valid Dice Face. You are staying on same position.");
+            } else if (position2< 0) {
+                position2 = source;
+                System.out.println("You came back to start.");
+            } else {
+                System.out.println("New Position of player2: " + position2);
             }
         }
-        System.out.println("You Won position is : "+position);
-        System.out.println("Number of times game played: "+diceRoll);
-
+        System.out.println("Player1's position is : "+position1);
+        System.out.println("Number of times game played : "+diceRoll1);
+        System.out.println("Player2's position is : "+position2);
+        System.out.println("Number of times game played : "+diceRoll2);
+        if (position1==100) {
+            System.out.println("Player1 has Won");
+        }else {
+            System.out.println("Player2 has Won");
     }
     }
+}
 
 
