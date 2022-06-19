afd13b9b7283650cf6d6c7bb87e803d17d737a76
Dipali <dipssonawane123@gmail.com>
2022-06-19 13:33:56 +0530

    Modified: player reach winnimg position functionality modified

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -1,30 +1,49 @@
-/* The Player then checks for a Option. They are No Play, Ladder or Snake.
- - Use ((RANDOM)) to check for Options - In Case of No Play the player stays in the sameposition
-- In Case of Ladder the player moves ahead by the number of position received in the die
-- In Case of Snake the player moves behind */
+/* Repeat till the Player reaches the winning position 100.
+- Note In case the player position moves below 0,
+then the player restarts from 0  */
 
 package com.snakeandladder;
 public class SnakeAndLadder {
+    final static int source = 0;
+    final  static  int destination = 100;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to snake and ladder");
-        int position=0;
-        int randomCheck=(int) Math.floor(Math.random() * 10) % 6+1;
-        int randomCheck2 = (int) (Math.random() * 10) % 3;
-        switch(randomCheck2) {
-            case 0:
-                System.out.println("No play");
-                position += 0;
+        int position=0, diceRoll=0;
+        while(position != destination)
+        {
+            diceRoll++;
+            int  randomCheck = (int) Math.floor(Math.random() * 10) % 6+1;
+            int  randomCheck2 = (int) (Math.random() * 10) % 3;
+            switch(randomCheck2) {
+                case 0:
+                    System.out.println("No play");
+                    position += 0;
+                    break;
+                case 1:
+                    System.out.println("Ladder");
+                    position += randomCheck;
+                    break;
+                case 2:
+                    System.out.println("Snake");
+                    position -= randomCheck;
+                    break;
+            }
+            if (position == 100) {
                 break;
-            case 1:
-                System.out.println("Ladder");
-                position += randomCheck;
-                break;
-            case 2:
-                System.out.println("Snake");
+            } else if (position > 100) {
                 position -= randomCheck;
-                break;
+                System.out.println("Invalid Dice Face. You are staying on same position.");
+            } else if (position < 0) {
+                position = source;
+                System.out.println("You came back to start.");
+            } else {
+                System.out.println("New Position: " + position);
+            }
+        }
+        System.out.println("You won");
+
 
     }
     }
-}
+
 
