ad09be7b6d5894be5403aa20ac18e38f47870289
Dipali <dipssonawane123@gmail.com>
2022-06-19 13:10:00 +0530

    Added:Snake and ladder

---
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,13 @@
+/* Snake and Ladder game played with single
+   player at start position 0 */
+
+package com.snakeandladder;
+
+public class SnakeAndLadder {
+
+    public static void main(String[] args) {
+            System.out.println("Welcome to snake and ladder");
+            int position =0;
+        }
+    }
+
