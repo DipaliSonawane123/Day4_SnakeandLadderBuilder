3b444ce3c156bb5a32edd64b606ee515de285673
Dipali <dipssonawane123@gmail.com>
2022-06-19 13:19:03 +0530

    Modified:diceroll functionality modified

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -1,13 +1,14 @@
-/* Snake and Ladder game played with single
-   player at start position 0 */
+/* /* The Player rolls the die to get a number between 1 to 6.
+ - Use ((RANDOM)) to get the number between 1 to 6 */
 
 package com.snakeandladder;
-
 public class SnakeAndLadder {
-
     public static void main(String[] args) {
-            System.out.println("Welcome to snake and ladder");
-            int position =0;
-        }
+        System.out.println("Welcome to snake and ladder");
+        int position=0;
+        int randomCheck=(int) Math.floor(Math.random() * 10) % 6+1;
+        System.out.println(randomCheck);
+
+    }
     }
 
