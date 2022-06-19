07a44f8cf25924a5090e25c6bdee2afaca9e75df
Dipali <dipssonawane123@gmail.com>
2022-06-19 13:27:09 +0530

    Modified:noplay snake ladder postion modified

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -1,5 +1,7 @@
-/* /* The Player rolls the die to get a number between 1 to 6.
- - Use ((RANDOM)) to get the number between 1 to 6 */
+/* The Player then checks for a Option. They are No Play, Ladder or Snake.
+ - Use ((RANDOM)) to check for Options - In Case of No Play the player stays in the sameposition
+- In Case of Ladder the player moves ahead by the number of position received in the die
+- In Case of Snake the player moves behind */
 
 package com.snakeandladder;
 public class SnakeAndLadder {
@@ -7,8 +9,22 @@ public class SnakeAndLadder {
         System.out.println("Welcome to snake and ladder");
         int position=0;
         int randomCheck=(int) Math.floor(Math.random() * 10) % 6+1;
-        System.out.println(randomCheck);
+        int randomCheck2 = (int) (Math.random() * 10) % 3;
+        switch(randomCheck2) {
+            case 0:
+                System.out.println("No play");
+                position += 0;
+                break;
+            case 1:
+                System.out.println("Ladder");
+                position += randomCheck;
+                break;
+            case 2:
+                System.out.println("Snake");
+                position -= randomCheck;
+                break;
 
     }
     }
+}
 
